eaea61d304e3848ecd34ae7a1dab2cce066f806d
nagamocha3000 <nagamocha@gmail.com>
2020-07-04 16:00:21 +0300

    make connect readable/serializable

--- internal/protocol/connectDisconnect.go
+++ internal/protocol/connectDisconnect.go
@@ -8,19 +8,18 @@ import (
 	CONNECT PACKET
 */
 type connectPacket struct {
-	protocolLevel    byte
-	usernamePresent  bool
-	passwordPresent  bool
-	willRetain       bool
-	willQoS          byte
-	willPresent      bool
-	cleanSession     bool
-	keepAlive        uint16
-	clientIdentifier []byte
-	willTopic        []byte
-	willMessage      []byte
-	username         []byte
-	password         []byte
+	protocolLevel       byte
+	loginDetailsPresent bool
+	willRetain          bool
+	willQoS             byte
+	willPresent         bool
+	cleanSession        bool
+	keepAlive           uint16
+	clientIdentifier    []byte
+	willTopic           []byte
+	willMessage         []byte
+	username            []byte
+	password            []byte
 }
 
 func (p *connectPacket) Read(b []byte) (n int, err error) {
@@ -31,17 +30,60 @@ func (p *connectPacket) Read(b []byte) (n int, err error) {
 	if lenConnectPacket > maxPayloadSize {
 		return 0, ErrInvalidPacket
 	}
-
-	return lenConnectPacket, io.EOF
+	// write fixed header
+	buf := newWritableBuf(b)
+	buf.WriteByte(connect<<4 | 0x0)
+	writePayloadSize(buf, uint32(p.payloadLen()))
+	// write protocol name + level version 3.11
+	buf.Write([]byte{0, 4, 'M', 'Q', 'T', 'T', 0x04})
+	// write connect flags
+	buf.WriteByte(p.getConnectFlagsByte())
+	// write keep alive (msb then lsb)
+	buf.WriteByte(byte(p.keepAlive >> 8))
+	buf.WriteByte(byte(p.keepAlive))
+	// write payload
+	buf.writeMQTTStr(p.clientIdentifier)
+	if p.willPresent {
+		buf.writeMQTTStr(p.willTopic)
+		buf.writeMQTTStr(p.willMessage)
+	}
+	if p.loginDetailsPresent {
+		buf.writeMQTTStr(p.username)
+		buf.writeMQTTStr(p.password)
+	}
+	return buf.bytesWritten(), io.EOF
 }
 
-func (p *connectPacket) Len() int {
+func (p *connectPacket) getConnectFlagsByte() byte {
+	var b byte = 0
+	if p.loginDetailsPresent { // username & password present
+		b = b | 0xC0 // 0x80 + 0x40 for both username & pass
+	}
+	if p.willRetain {
+		b = b | 0x20
+	}
+	b = b | p.willQoS
+	if p.willPresent {
+		b = b | 0x04
+	}
+	if p.cleanSession {
+		b = b | 0x02
+	}
+	return b
+}
+
+func (p *connectPacket) payloadLen() int {
 	payloadLen := 10 + // variable Header
 		2 + len(p.clientIdentifier) +
 		2 + len(p.willTopic) +
 		2 + len(p.willMessage) +
 		2 + len(p.username) +
 		2 + len(p.password)
+	return payloadLen
+}
+
+func (p *connectPacket) Len() int {
+	payloadLen := p.payloadLen()
 	return 1 + // control pkt type + flags
 		lenPayloadSizeField(payloadLen) + // remaining Length field
 		payloadLen // variable header + payload
