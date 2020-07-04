a510a602a9df43e2354fd6780b1d0374b09d36cd
nagamocha3000 <nagamocha@gmail.com>
2020-07-04 13:07:30 +0300

    pingreq, pingresp pkts readable

--- internal/protocol/ping.go
+++ internal/protocol/ping.go
@@ -1,5 +1,41 @@
 package protocol
 
+import "io"
+
+/*
+	PING REQUEST PACKET
+*/
 type pingreqPacket struct{}
 
+func (p *pingreqPacket) Read(buf []byte) (n int, err error) {
+	if len(buf) < 2 { // requires 2 bytes ? or more
+		return 0, io.ErrShortBuffer
+	}
+	buf[0] = pingreq<<4 | 0x0 // ctrl pkt type + flags(reserved)
+	buf[1] = 0                // no payload
+	return 2, io.EOF
+}
+
+func (p *pingreqPacket) Len() int {
+	// takes up 2 bytes
+	return 2
+}
+
+/*
+	PING RESPONSE PACKET
+*/
 type pingrespPacket struct{}
+
+func (p *pingrespPacket) Read(buf []byte) (n int, err error) {
+	if len(buf) < 2 { // requires 2 bytes ? or more
+		return 0, io.ErrShortBuffer
+	}
+	buf[0] = pingresp<<4 | 0x0 // ctrl pkt type + flags(reserved)
+	buf[1] = 0                 // no payload
+	return 2, io.EOF
+}
+
+func (p *pingrespPacket) Len() int {
+	// takes up 2 bytes
+	return 2
+}
